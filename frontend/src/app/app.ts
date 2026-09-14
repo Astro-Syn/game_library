4d3ad52bc6d4432a98816aa6212cb71b2b0b272b
Astro-Syn <kelseyamyb@gmail.com>
2026-09-14 12:14:54 -0400

    first commit

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('frontend');
+
+  constructor(private http: HttpClient) {}
+
+  getMessage() {
+  this.http.get<{ message: string }>('http://localhost:8000/api/hello')
+    .subscribe(response => {
+      console.log(response.message);
+    });
+}
+}
