ed5581bbd9e14acda071c9184d424f12174cada8
Astro-Syn <kelseyamyb@gmail.com>
2026-09-15 13:41:36 -0400

    update backend stuff

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from database import engine
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from database import engine, SessionLocal
+from models import Base, Game
+
 
 app = FastAPI()
 
-try:
-    with engine.connect() as connection:
-        print("Database connection successful!")
-except Exception as error:
-    print("Database connection failed:", error)
+Base.metadata.create_all(bind=engine)
+
+
+class GameCreate(BaseModel):
+    title: str
+    genre: str | None = None
+    platform: str | None = None
 
 
 app.add_middleware(
@@ -23,3 +30,23 @@ app.add_middleware(
 @app.get("/api/hello")
 def hello():
     return {"message": "Hello from Python!"}
+
+
+@app.post("/api/games")
+def create_game(game: GameCreate):
+
+    db: Session = SessionLocal()
+
+    new_game = Game(
+        title=game.title,
+        genre=game.genre,
+        platform=game.platform
+    )
+
+    db.add(new_game)
+    db.commit()
+    db.refresh(new_game)
+
+    db.close()
+
+    return new_game
---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from database import engine
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class Game(Base):
+    __tablename__ = "games"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    genre = Column(String)
+    platform = Column(String)
